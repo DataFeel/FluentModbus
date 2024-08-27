e4139ce75dd11d4f0aba3dddf214b4093923d16d
Charles Lee <clee781@gmail.com>
2024-08-27 03:17:22 -0500

    connection init rework

--- src/FluentModbus/Client/ModbusRtuClient.cs
+++ src/FluentModbus/Client/ModbusRtuClient.cs
@@ -9,8 +9,12 @@ namespace FluentModbus
     {
         #region Field
 
-        private (IModbusRtuSerialPort Value, bool IsInternal)? _serialPort;
+        //private (IModbusRtuSerialPort Value, bool IsInternal)? _serialPort;
+        private IModbusRtuSerialPort _serialPort = default!;
         private ModbusFrameBuffer _frameBuffer = default!;
+
+        public SerialPort SystemPort { get; private set; }
+
 
         #endregion
 
@@ -19,9 +23,33 @@ namespace FluentModbus
         /// <summary>
         /// Creates a new Modbus RTU client for communication with Modbus RTU servers or bridges, routers and gateways for communication with TCP end units.
         /// </summary>
-        public ModbusRtuClient()
+        public ModbusRtuClient(IModbusRtuSerialPort serialPort)
         {
-            //
+            _serialPort = serialPort;
+        }
+
+        public ModbusRtuClient(string portString) : this(new SerialParameters(portString))
+        {
+        }
+
+        public ModbusRtuClient(SerialParameters serialParams)
+        {
+            var systemPort = new SerialPort(serialParams.Port)
+            {
+                BaudRate = serialParams.BaudRate,
+                Handshake = serialParams.Handshake,
+                Parity = serialParams.Parity,
+                StopBits = serialParams.StopBits,
+                ReadTimeout = serialParams.ReadTimeout,
+                WriteTimeout = serialParams.WriteTimeout
+            };
+            SystemPort = systemPort;
+
+            systemPort.Open();
+            systemPort.BaseStream.Flush();
+            systemPort.Close();
+
+            _serialPort = new ModbusRtuSerialPort(systemPort);
         }
 
         #endregion
@@ -31,126 +59,67 @@ namespace FluentModbus
         /// <summary>
         /// Gets the connection status of the underlying serial port.
         /// </summary>
-        public override bool IsConnected => _serialPort?.Value.IsOpen ?? false;
+        public override bool IsConnected => _serialPort.IsOpen;
 
-        /// <summary>
-        /// Gets or sets the serial baud rate. Default is 9600.
-        /// </summary>
-        public int BaudRate { get; set; } = 9600;
-
-        /// <summary>
-        /// Gets or sets the handshaking protocol for serial port transmission of data. Default is Handshake.None.
-        /// </summary>
-        public Handshake Handshake { get; set; } = Handshake.None;
-
-        /// <summary>
-        /// Gets or sets the parity-checking protocol. Default is Parity.Even.
-        /// </summary>
-        // Must be even according to the spec (https://www.modbus.org/docs/Modbus_over_serial_line_V1_02.pdf,
-        // section 2.5.1 RTU Transmission Mode): "The default parity mode must be even parity."
-        public Parity Parity { get; set; } = Parity.Even;
-
-        /// <summary>
-        /// Gets or sets the standard number of stopbits per byte. Default is StopBits.One.
-        /// </summary>
-        public StopBits StopBits { get; set; } = StopBits.One;
-
-        /// <summary>
-        /// Gets or sets the read timeout in milliseconds. Default is 1000 ms.
-        /// </summary>
-        public int ReadTimeout { get; set; } = 1000;
-
-        /// <summary>
-        /// Gets or sets the write timeout in milliseconds. Default is 1000 ms.
-        /// </summary>
-        public int WriteTimeout { get; set; } = 1000;
 
         #endregion
 
         #region Methods
 
-        /// <summary>
-        /// Connect to the specified <paramref name="port"/> with <see cref="ModbusEndianness.LittleEndian"/> as default byte layout.
-        /// </summary>
-        /// <param name="port">The COM port to be used, e.g. COM1.</param>
-        public void Connect(string port)
-        {
-            Connect(port, ModbusEndianness.LittleEndian);
-        }
+
 
         /// <summary>
         /// Connect to the specified <paramref name="port"/>.
         /// </summary>
         /// <param name="port">The COM port to be used, e.g. COM1.</param>
         /// <param name="endianness">Specifies the endianness of the data exchanged with the Modbus server.</param>
-        public void Connect(string port, ModbusEndianness endianness)
+        public void Connect(ModbusEndianness endianness = ModbusEndianness.BigEndian)
         {
-            var serialPort = new ModbusRtuSerialPort(new SerialPort(port)
+
+            SwapBytes =
+                BitConverter.IsLittleEndian && endianness == ModbusEndianness.BigEndian ||
+                !BitConverter.IsLittleEndian && endianness == ModbusEndianness.LittleEndian;
+
+            _frameBuffer = new ModbusFrameBuffer(256);
+
+            _serialPort.Close();
+
+            if (!_serialPort.IsOpen)
             {
-                BaudRate = BaudRate,
-                Handshake = Handshake,
-                Parity = Parity,
-                StopBits = StopBits,
-                ReadTimeout = ReadTimeout,
-                WriteTimeout = WriteTimeout
-            });
+                _serialPort.Open();
+            }
 
-            Initialize(serialPort, isInternal: true, endianness);
+            //if (_serialPort.GetType() == typeof(ModbusRtuSerialPort))
+            //{
+            //    (_serialPort as ModbusRtuSerialPort).
+            //}
+
+            //var dump = _serialPort.ToString();
         }
 
-        public void Connect(IModbusRtuSerialPort serial)
-        {
-            Connect(serial, ModbusEndianness.LittleEndian);
-        }
 
-        public void Connect(IModbusRtuSerialPort serial, ModbusEndianness endianness)
-        {
-            Initialize(serial, isInternal: false, endianness);
-        }
+ 
 
         /// <summary>
         /// Initialize the Modbus TCP client with an externally managed <see cref="IModbusRtuSerialPort"/>.
         /// </summary>
         /// <param name="serialPort">The externally managed <see cref="IModbusRtuSerialPort"/>.</param>
         /// <param name="endianness">Specifies the endianness of the data exchanged with the Modbus server.</param>
-        public void Initialize(IModbusRtuSerialPort serialPort, ModbusEndianness endianness)
-        {
-            Initialize(serialPort, isInternal: false, endianness);
-        }
+        //public void Initialize(IModbusRtuSerialPort serial, ModbusEndianness endianness)
+        //{
+        //    Initialize(serial, endianness);
+        //}
 
-        private void Initialize(IModbusRtuSerialPort serialPort, bool isInternal, ModbusEndianness endianness)
-        {
-            /* According to the spec (https://www.modbus.org/docs/Modbus_over_serial_line_V1_02.pdf), 
-             * section 2.5.1 RTU Transmission Mode: "... the use of no parity requires 2 stop bits."
-             * Remove this check to improve compatibility (#56).
-             */
-
-            //if (Parity == Parity.None && StopBits != StopBits.Two)
-            //    throw new InvalidOperationException(ErrorMessage.Modbus_NoParityRequiresTwoStopBits);
-
-            SwapBytes = 
-                BitConverter.IsLittleEndian && endianness == ModbusEndianness.BigEndian ||
-                !BitConverter.IsLittleEndian && endianness == ModbusEndianness.LittleEndian;
-
-            _frameBuffer = new ModbusFrameBuffer(256);
-
-            if (_serialPort.HasValue && _serialPort.Value.IsInternal)
-                _serialPort.Value.Value.Close();
-
-            _serialPort = (serialPort, isInternal);
-
-            if (!serialPort.IsOpen)
-                serialPort.Open(); 
-        }
 
         /// <summary>
         /// Closes the opened COM port and frees all resources.
         /// </summary>
         public void Close()
         {
-            if (_serialPort.HasValue && _serialPort.Value.IsInternal)
-                _serialPort.Value.Value.Close();
-
+            if (_serialPort != null)
+            {
+                _serialPort.Close();
+            }
             _frameBuffer?.Dispose();
         }
 
@@ -195,7 +164,7 @@ namespace FluentModbus
             frameLength = (int)_frameBuffer.Writer.BaseStream.Position;
 
             // send request
-            _serialPort!.Value.Value.Write(_frameBuffer.Buffer, 0, frameLength);
+            _serialPort.Write(_frameBuffer.Buffer, 0, frameLength);
 
             // special case: broadcast (only for write commands)
             if (unitIdentifier == 0)
@@ -207,7 +176,7 @@ namespace FluentModbus
 
             while (true)
             {
-                frameLength += _serialPort!.Value.Value.Read(_frameBuffer.Buffer, frameLength, _frameBuffer.Buffer.Length - frameLength);
+                frameLength += _serialPort.Read(_frameBuffer.Buffer, frameLength, _frameBuffer.Buffer.Length - frameLength);
 
                 if (ModbusUtils.DetectResponseFrame(unitIdentifier, _frameBuffer.Buffer.AsMemory()[..frameLength]))
                 {
@@ -265,5 +234,21 @@ namespace FluentModbus
         }
 
         #endregion
+    }
+
+    public readonly struct SerialParameters
+    {
+        public readonly string Port { get; }
+        public readonly int BaudRate { get; } = 115200;
+        public readonly Handshake Handshake { get; } = Handshake.None;
+        public readonly Parity Parity { get; } = Parity.None;
+        public readonly StopBits StopBits { get; } = StopBits.One;
+        public readonly int ReadTimeout { get; } = 250;
+        public readonly int WriteTimeout { get; } = 250;
+
+        public SerialParameters(string port)
+        {
+            Port = port;
+        }
     }
 }
--- src/FluentModbus/Client/ModbusRtuClientAsync.cs
+++ src/FluentModbus/Client/ModbusRtuClientAsync.cs
@@ -47,7 +47,7 @@
 
 
             // send request
-            await _serialPort!.Value.Value.WriteAsync(_frameBuffer.Buffer, 0, frameLength, cancellationToken).ConfigureAwait(false);
+            await _serialPort.WriteAsync(_frameBuffer.Buffer, 0, frameLength, cancellationToken).ConfigureAwait(false);
 
             // special case: broadcast (only for write commands)
             if (unitIdentifier == 0 || unitIdentifier > 100)
@@ -59,7 +59,7 @@
 
             while (true)
             {
-                frameLength += await _serialPort!.Value.Value.ReadAsync(_frameBuffer.Buffer, frameLength, _frameBuffer.Buffer.Length - frameLength, cancellationToken).ConfigureAwait(false);
+                frameLength += await _serialPort.ReadAsync(_frameBuffer.Buffer, frameLength, _frameBuffer.Buffer.Length - frameLength, cancellationToken).ConfigureAwait(false);
 
                 if (ModbusUtils.DetectResponseFrame(unitIdentifier, _frameBuffer.Buffer.AsMemory()[..frameLength]))
                 {
